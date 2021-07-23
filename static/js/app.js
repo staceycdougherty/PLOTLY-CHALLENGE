892976b5ccb2671ac0a824350afafb553c34e585
Stacey Dougherty <staceydougherty@Staceys-MacBook-Air.local>
2021-07-22 20:03:39 -0700

    testing with console.log

---
+++ static/js/app.js
@@ -0,0 +1,17 @@
+// read json file with D3
+d3.json("data/samples.json").then((data) => {
+    console.log(data);
+
+    var data = data;
+//add "names" to drop down menu
+    var names = data.names;
+
+    names.forEach((name) => {
+		d3.select("#selDataset").append("option").text(name);
+	})
+
+//Initialize page wit default plots
+function init() {
+    defaultDataset = data.samples.filter(sample => sample.id === "940")[0];
+    console.log(defaultDataset);
+}
